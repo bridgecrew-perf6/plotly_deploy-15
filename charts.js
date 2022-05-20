43f65fd41fd6cfb1414eac450e7cf6d830058e2d
bryonlhobbs <99457275+bryonlhobbs@users.noreply.github.com>
2022-05-19 21:58:30 -0500

    Testing yticks

--- charts.js
+++ charts.js
@@ -68,13 +68,12 @@ function buildCharts(sample) {
     var otuIds = selectedSample.otu_ids;
     var otuLabels = selectedSample.otu_labels;
     var sampleValues = selectedSample.sample_values;
-    console.log(otuIds);
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
     var yticks = otuIds.map(object => object.sampleValues).sort((a,b) => b.sampleValues - a.sampleValues).slice(0,10);
-
+    console.log(yticks);
     // 8. Create the trace for the bar chart. 
     var barData = {
       x: sampleValues,
